5a482d6b8390728cc351ab51054a9fe500c79152
junwang-zingbox <junwang@zingbox.com>
2020-05-02 07:43:02 -0700

    start over

---
+++ build/precache-manifest.3f278a5880b42caea2ef41245f2ea3b9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "96ca09f3f45a0993c3b6240adae7748a",
+    "url": "/build/index.html"
+  },
+  {
+    "revision": "828e145da10eebb30a96",
+    "url": "/build/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "fbbb388a05bee0c24e3b",
+    "url": "/build/static/js/2.d7f13e62.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/build/static/js/2.d7f13e62.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "828e145da10eebb30a96",
+    "url": "/build/static/js/main.e9a0200d.chunk.js"
+  },
+  {
+    "revision": "e126d2a7409836e34622",
+    "url": "/build/static/js/runtime-main.e016fb1a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/build/static/media/logo.5d5d9eef.svg"
+  }
+]);
