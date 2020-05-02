67d301bb0920e84b338391e9d8e4a3618a019f7c
junwang-zingbox <junwang@zingbox.com>
2020-05-02 08:57:26 -0700

    script added to push

---
+++ precache-manifest.34410a4f00dcc9d1bc50fecafb93a8d7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "25bfe0c18a4dde10e240d092c0e3145f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "22eabf18ed6d513d8a2e",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "19c8317b28e6baabaacf",
+    "url": "/static/js/2.f2bdcfd0.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.f2bdcfd0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "22eabf18ed6d513d8a2e",
+    "url": "/static/js/main.b10ae8dd.chunk.js"
+  },
+  {
+    "revision": "196efde3c5d3236b630c",
+    "url": "/static/js/runtime-main.85f73532.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
